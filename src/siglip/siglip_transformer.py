a98a241cd476f20e85c3966777129a3111f42f6f
arya2004 <arya20j4@gmail.com>
2025-02-03 02:35:52 +0530

    Add SiglipTransformer class for processing pixel values

---
+++ src/siglip/siglip_transformer.py
@@ -0,0 +1,25 @@
+from typing import Optional, Tuple
+import torch
+import torch.nn as nn
+
+from siglip.siglip_config import SiglipConfig
+
+
+class SiglipTransformer(nn.Module):
+
+    def __init__(self, config: SiglipConfig):
+        super().__init__()
+
+        self.config = config
+        embedding_dimension = config.hidden_size
+        self.embeddings = SiglipEmbeddings(config)
+        self.encoder = SiglipEncoder(config)
+        self.post_layernorm = nn.LayerNorm(embedding_dimension, eps=config.layer_norm_eps)
+
+    # converts [batchSize, channel, height, width] to [batchSize, num of patches, embedding dimension]
+    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
+        
+        hidden_states = self.embeddings(pixel_values)
+        last_hidden_state = self.encoder(input_embedding=hidden_states)
+        last_hidden_state= self.post_layernorm(last_hidden_state)
+        return last_hidden_state
