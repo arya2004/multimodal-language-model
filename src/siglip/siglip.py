364a1d273219d6ca3d6bf01c6d2ec80fbcda05f1
arya2004 <arya20j4@gmail.com>
2025-02-03 02:35:26 +0530

    Add Siglip class for vision model integration

---
+++ src/siglip/siglip.py
@@ -0,0 +1,17 @@
+from typing import Optional, Tuple
+import torch
+import torch.nn as nn
+
+from siglip.siglip_config import SiglipConfig
+from siglip.siglip_transformer import SiglipTransformer
+
+class Siglip(nn.Module):
+
+    def __init__(self, config: SiglipConfig):
+        super().__init__()
+        self.config = config  
+        self.vision_model = SiglipTransformer(config)
+
+    # converts [batchSize, channel, height, width] to [batchSize, num of patches, embedding dimension]
+    def forward(self, pixel_value) -> Tuple:
+        return self.vision_model(pixel_value=pixel_value)
